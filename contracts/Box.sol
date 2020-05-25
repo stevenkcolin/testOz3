ca162afedbdeabc4786fa2bd3d9688b904ff920c
stevenkcolin <steven.k.colin@gmail.com>
2020-05-25 08:49:36 +0800

    submit

--- contracts/Box.sol
+++ contracts/Box.sol
@@ -2,20 +2,22 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.6.0;
 
+// Import Ownable from the OpenZeppelin Contracts library
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Box {
+
+// Make Box inherit from the Ownable contract
+contract Box is Ownable {
     uint256 private value;
 
-    // Emitted when the stored value changes
     event ValueChanged(uint256 newValue);
 
-    // Stores a new value in the contract
-    function store(uint256 newValue) public {
+    // The onlyOwner modifier restricts who can call the store function
+    function store(uint256 newValue) public onlyOwner {
         value = newValue;
         emit ValueChanged(newValue);
     }
 
-    // Reads the last stored value
     function retrieve() public view returns (uint256) {
         return value;
     }
